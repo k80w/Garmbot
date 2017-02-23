47bc7a8adbdcde5971902085b466b2b8d8888e30
FlyingSixtySix <andrewblack2656@gmail.com>
2017-02-23 14:45:52 -0500

    Coverage of NaN/no numbers.

--- plugins/math/index.js
+++ plugins/math/index.js
@@ -3,9 +3,17 @@ const debug = require("debug")("garmbot:modules:math"),
 
 module.exports = function(garmbot) {
 	garmbot.addCommand(["round"], async function(message, args) {
-		let embed = new Discord.RichEmbed()
-			.setTitle("Rounded number!")
-			.setDescription(Math.round(Number(args.trim())))
-		return message.channel.sendEmbed(embed, message.author.toString());
+		var num = Number(args.trim());
+		if (num == undefined) {
+			let embed = new Discord.RichEmbed()
+				.setTitle("Rounded number!")
+				.setDescription("You didn't specify a number!");
+			return message.channel.sendEmbed(embed, message.author.toString());
+		} else {
+			let embed = new Discord.RichEmbed()
+				.setTitle("Rounded number!")
+				.setDescription(Math.round(num))
+			return message.channel.sendEmbed(embed, message.author.toString());
+		}
 	});
 }
