7c423100ab2212d01a49bbe1f5230d679785557e
FlyingSixtySix <andrewblack2656@gmail.com>
2017-02-23 14:27:11 -0500

    Math Rounding
    Added a simpistic math rounding tool.

---
+++ plugins/math/index.js
@@ -0,0 +1,13 @@
+const debug = require("debug")("garmbot:math"),
+	Discord = require("discord.js");
+
+module.exports = function(garmbot) {
+	garmbot.addCommand(["round"], async function(message, args) {
+		let numToRound = args.trim();
+		let embed = new Discord.RichEmbed();
+		embed
+			.setTitle("Rounded number!")
+			.setDescription(Math.round(numToRound))
+		return message.channel.sendEmbed(embed, message.author.toString());
+	});
+}
