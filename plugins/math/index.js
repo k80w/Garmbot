382bc0444c82ceea93c59d6949a572616f29f630
FlyingSixtySix <andrewblack2656@gmail.com>
2017-02-23 14:33:16 -0500

    Minified code a little.

--- plugins/math/index.js
+++ plugins/math/index.js
@@ -1,13 +1,11 @@
-const debug = require("debug")("garmbot:math"),
+const debug = require("debug")("garmbot:modules:math"),
 	Discord = require("discord.js");
 
 module.exports = function(garmbot) {
 	garmbot.addCommand(["round"], async function(message, args) {
-		let numToRound = args.trim();
-		let embed = new Discord.RichEmbed();
-		embed
+		let embed = new Discord.RichEmbed()
 			.setTitle("Rounded number!")
-			.setDescription(Math.round(numToRound))
+			.setDescription(Math.round(Number(args.trim())))
 		return message.channel.sendEmbed(embed, message.author.toString());
 	});
 }
